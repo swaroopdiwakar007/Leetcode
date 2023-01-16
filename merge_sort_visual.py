a085d013d8fe9c4715f3f55e8a55f0d9d5601c8c
SPDiw <swaroopdiwakar007@gmail.com>
2023-01-16 17:11:09 +0530

    Create merge_sort_visual.py

---
+++ merge_sort_visual.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import random
+
+random.seed("ABC")
+
+amount = 100
+
+numbers = [random.randint(0, 1000) for _ in range(amount)]
+
+print(numbers)
+
+
+def merge_sort(numbers_list, left, right):
+    if left >= right:
+        return
+
+    mid = (left + right) // 2
+
+    plt.bar(list(range(amount)), numbers_list)
+    plt.pause(0.001)
+    plt.clf()
+
+    merge_sort(numbers_list, left, mid)
+    merge_sort(numbers_list, mid + 1, right)
+
+    plt.bar(list(range(amount)), numbers_list)
+    plt.pause(0.001)
+    plt.clf()
+
+    merge(numbers_list, left, right, mid)
+
+    plt.bar(list(range(amount)), numbers_list)
+    plt.pause(0.001)
+    plt.clf()
+
+
+def merge(numbers_list, left, right, mid):
+    left_copy = numbers_list[left: mid + 1]
+    right_copy = numbers_list[mid + 1: right + 1]
+
+    l, r = 0, 0
+    sorted = left
+
+    while l < len(left_copy) and r < len(right_copy):
+        if left_copy[l] <= right_copy[r]:
+            numbers_list[sorted] = left_copy[l]
+            l += 1
+
+        else:
+            numbers_list[sorted] = right_copy[r]
+            r += 1
+
+        sorted += 1
+
+    while l < len(left_copy):
+        numbers_list[sorted] = left_copy[l]
+        l += 1
+        sorted += 1
+    while r < len(right_copy):
+        numbers_list[sorted] = right_copy[r]
+        sorted += 1
+        r += 1
+
+
+merge_sort(numbers, 0, len(numbers) - 1)
+print(numbers)
+plt.bar(list(range(amount)), numbers)
+plt.show()
